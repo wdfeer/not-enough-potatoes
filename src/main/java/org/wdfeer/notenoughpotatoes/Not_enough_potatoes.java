a14f84bd59ed8b09cbb4389a6d9b7a4ddfc54934
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-06-12 19:11:35 +0200

    Fix build errors

---
+++ src/main/java/org/wdfeer/notenoughpotatoes/Not_enough_potatoes.java
@@ -0,0 +1,10 @@
+package org.wdfeer.notenoughpotatoes;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Not_enough_potatoes implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/org/wdfeer/notenoughpotatoes/client/Not_enough_potatoesClient.java
@@ -0,0 +1,1 @@
+package org.wdfeer.notenoughpotatoes.client;import net.fabricmc.api.ClientModInitializer;public class Not_enough_potatoesClient implements ClientModInitializer {    @Override    public void onInitializeClient() {    }}
