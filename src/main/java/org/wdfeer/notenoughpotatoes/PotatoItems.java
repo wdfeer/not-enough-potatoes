14f4a3d032d48668aa3e7f3a7ebb94c9b832923d
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-06-13 18:01:37 +0200

    Add all potato armor pieces

--- src/main/java/org/wdfeer/notenoughpotatoes/PotatoItems.java
+++ src/main/java/org/wdfeer/notenoughpotatoes/PotatoItems.java
@@ -3,14 +3,22 @@ package org.wdfeer.notenoughpotatoes;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 
 public class PotatoItems {
-    public static Item[] items = new Item[] {
-            new PotatoArmorPiece(ArmorItem.Type.HELMET)
-    };
+    public static Item[] items;
+
+    private static Identifier GetIdentifier(String name){
+        return new Identifier(PotatoMod.MOD_ID, name);
+    }
+
     public static void Initialize(){
-        for (Item item : items) {
-            Registries.ITEM.createEntry(item);
-        }
+        items = new Item[] {
+                Registry.register(Registries.ITEM, GetIdentifier("potato_helmet"), new PotatoArmorPiece(ArmorItem.Type.HELMET)),
+                Registry.register(Registries.ITEM, GetIdentifier("potato_chestplate"), new PotatoArmorPiece(ArmorItem.Type.CHESTPLATE)),
+                Registry.register(Registries.ITEM, GetIdentifier("potato_leggings"), new PotatoArmorPiece(ArmorItem.Type.LEGGINGS)),
+                Registry.register(Registries.ITEM, GetIdentifier("potato_boots"), new PotatoArmorPiece(ArmorItem.Type.BOOTS))
+        };
     }
 }
