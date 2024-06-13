49e9ab2f599f9b0a57fac194545ad390c62d6b57
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-06-13 17:43:18 +0200

    Add ItemMixin

--- src/main/java/org/wdfeer/notenoughpotatoes/PotatoArmorPiece.java
+++ src/main/java/org/wdfeer/notenoughpotatoes/PotatoArmorPiece.java
@@ -1,10 +1,16 @@
 package org.wdfeer.notenoughpotatoes;
 
 import net.minecraft.item.ArmorItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.Items;
 import net.minecraft.registry.Registries;
 
 public class PotatoArmorPiece extends ArmorItem {
     public PotatoArmorPiece(ArmorItem.Type type) {
         super(PotatoMaterial.armorMaterialRegistryEntry, type, new Settings());
+    }
+
+    public void OnPotatoEaten(){
+
     }
 }
---
+++ src/main/java/org/wdfeer/notenoughpotatoes/mixin/ItemMixin.java
@@ -0,0 +1,32 @@
+package org.wdfeer.notenoughpotatoes.mixin;
+
+import net.minecraft.component.DataComponentTypes;
+import net.minecraft.component.type.FoodComponent;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.wdfeer.notenoughpotatoes.PotatoArmorPiece;
+
+@Mixin(Item.class)
+public class ItemMixin {
+    @Inject(method = "use", at = @At("HEAD"))
+    private void injectItemUse(World world, PlayerEntity user, Hand hand, CallbackInfoReturnable<TypedActionResult<ItemStack>> cir){
+        ItemStack itemStack = user.getStackInHand(hand);
+        FoodComponent foodComponent = (FoodComponent)itemStack.get(DataComponentTypes.FOOD);
+        if (itemStack.getItem() == Items.POTATO && foodComponent != null && user.canConsume(foodComponent.canAlwaysEat())) {
+            user.getArmorItems().forEach((ItemStack s) -> {
+                if (s.getItem() instanceof PotatoArmorPiece potatoArmor) {
+                    potatoArmor.OnPotatoEaten();
+                }
+            });
+        }
+    }
+}
