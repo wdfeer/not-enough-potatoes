a0835d4d2a869705590eb7bcf65a35b86724bdb2
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-06-15 07:59:47 +0200

    Fix textures

--- src/main/java/org/wdfeer/not_enough_potatoes/PotatoMaterial.java
+++ src/main/java/org/wdfeer/not_enough_potatoes/PotatoMaterial.java
@@ -22,16 +22,18 @@ public class PotatoMaterial {
         defenseMap.put(ArmorItem.Type.LEGGINGS, 2);
         defenseMap.put(ArmorItem.Type.BOOTS, 1);
 
+        Identifier id = new Identifier(PotatoMod.MOD_ID, "potato");
+
         material = new ArmorMaterial(defenseMap,
                 8,
                 SoundEvents.ITEM_ARMOR_EQUIP_GENERIC,
                 () -> Ingredient.ofItems(Items.POTATO),
                 List.of(new ArmorMaterial.Layer[]{
-                        new ArmorMaterial.Layer(new Identifier(PotatoMod.MOD_ID, "potato_armor"))
+                        new ArmorMaterial.Layer(id)
                 }),
                 0F,
                 0F);
 
-        Registry.register(Registries.ARMOR_MATERIAL, new Identifier(PotatoMod.MOD_ID, "potato_armor_material"), material);
+        Registry.register(Registries.ARMOR_MATERIAL, id, material);
     }
 }
