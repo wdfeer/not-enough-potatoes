52d7f35f29dc248e78c1f91bd0e9e7f4e847fd1b
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-06-15 10:26:11 +0200

    Fix OnPotatoEaten called multiple times

--- src/main/java/org/wdfeer/not_enough_potatoes/mixin/ItemMixin.java
+++ src/main/java/org/wdfeer/not_enough_potatoes/mixin/ItemMixin.java
@@ -1,26 +1,23 @@
 package org.wdfeer.not_enough_potatoes.mixin;
 
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
-import net.minecraft.util.Hand;
-import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.wdfeer.not_enough_potatoes.PotatoArmorPiece;
-import org.wdfeer.not_enough_potatoes.PotatoMod;
 
 @Mixin(Item.class)
 public class ItemMixin {
-    @Inject(method = "use(Lnet/minecraft/world/World;Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/util/Hand;)Lnet/minecraft/util/TypedActionResult;", at = @At("RETURN"))
-    private void injectItemUse(World world, PlayerEntity user, Hand hand, CallbackInfoReturnable<TypedActionResult<ItemStack>> cir){
-        ItemStack itemStack = user.getStackInHand(hand);
-        if (itemStack.isOf(Items.POTATO)) {
-            PotatoArmorPiece.OnPotatoEaten(user);
+    @Inject(method = "finishUsing", at = @At("RETURN"))
+    private void injectFinishUsing(ItemStack stack, World world, LivingEntity user, CallbackInfoReturnable<ItemStack> cir){
+        if (stack.isOf(Items.POTATO) && user instanceof PlayerEntity player) {
+            PotatoArmorPiece.OnPotatoEaten(player);
         }
     }
 }
