f67b995ef03ecd32e63ca3c65716770fc1ebb834
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-06-15 11:18:26 +0200

    Add tooltip

--- src/main/java/org/wdfeer/not_enough_potatoes/PotatoArmorPiece.java
+++ src/main/java/org/wdfeer/not_enough_potatoes/PotatoArmorPiece.java
@@ -14,6 +14,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.text.Text;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -45,7 +46,10 @@ public class PotatoArmorPiece extends ArmorItem {
 
         nbt.remove("AttributeModifiers");
         stack.addAttributeModifier(EntityAttributes.GENERIC_ARMOR, modifier, slot);
+    }
 
-//        appendTooltip(stack, world, List.of(new Text[]{ Text.of("Potatoes eaten: " + potatoes)}), TooltipContext.Default.NORMAL);
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        tooltip.add(Text.of("Potatoes eaten: " + stack.getOrCreateNbt().get("potatoes_eaten")));
     }
 }
