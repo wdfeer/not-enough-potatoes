b451942c055b65b91b53b6c59643281bb59af59d
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-06-20 16:29:41 +0200

    [WIP] Add PotatoAdapterSmithingRecipe

---
+++ src/main/java/org/wdfeer/not_enough_potatoes/recipe/PotatoAdapterSmithingRecipe.java
@@ -0,0 +1,12 @@
+package org.wdfeer.not_enough_potatoes.recipe;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.recipe.SmithingRecipe;
+import net.minecraft.util.Identifier;
+
+public class PotatoAdapterSmithingRecipe extends SmithingRecipe {
+    public PotatoAdapterSmithingRecipe(Identifier id, Ingredient base, Ingredient addition, ItemStack result) {
+        super(id, base, addition, result);
+    }
+}
