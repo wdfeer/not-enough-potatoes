2c5676a25dc1ebfb299390a85892b2dfcf2d67d2
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-06-30 18:43:13 +0200

    Add logarithm note

--- src/main/java/org/wdfeer/not_enough_potatoes/config/ProtectionConfig.java
+++ src/main/java/org/wdfeer/not_enough_potatoes/config/ProtectionConfig.java
@@ -3,9 +3,12 @@ package org.wdfeer.not_enough_potatoes.config;
 import eu.midnightdust.lib.config.MidnightConfig;
 
 public class ProtectionConfig extends MidnightConfig {
-    @Entry(category = "numbers", min = 1.1, max = 1e6)
+    @Comment
+    public static Comment logarithmNote = null;
+
+    @Entry(min = 1.1, max = 1e6)
     public static double potatoArmorLogBase = 1;
 
-    @Entry(category = "numbers", min = 1.1, max = 1e6)
+    @Entry(min = 1.1, max = 1e6)
     public static double potatoShardLogBase = 1;
 }
